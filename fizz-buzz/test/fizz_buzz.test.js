39cf393ce7e586f9e198008db5af454244d615c9
Concha Asensio <concha@codium.team>
2021-08-11 09:54:03 +0200

    FizzBuzz should return number 2.

--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -19,4 +19,14 @@ describe('FizzBuzz', () => {
     expect(Array.isArray(fizzBuzzNumbers)).toBe(true);
 
   });
+  it('should return number 2', () => {
+
+    const fizzBuzz = new FizzBuzz();
+
+    const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+
+    expect(fizzBuzzNumbers[1]).toBe('2');
+
+  });
+
 });
