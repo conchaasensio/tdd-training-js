dcfdf21f9a83d732fd21d33b4294ee07c665e9d9
Concha Asensio <concha@codium.team>
2021-08-11 10:33:25 +0200

    FizzBuzz should return Fizz instead of 6.

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -4,7 +4,7 @@ class FizzBuzz {
         for (let index = 0; index < 100; index ++) {
             const number = index + 1;
 
-            if (number === 3) {
+            if (number === 3 || number === 6) {
                 numbers.push('Fizz');
             } else {
                 numbers.push(number.toString());
