70148a4495ba52e612ffc65500b38ec5f166de0f
Concha Asensio <concha@codium.team>
2021-08-12 11:17:08 +0200

    Refactor: simplify code.

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,20 +1,17 @@
 class RomanNumerals {
   convertNumerals(number) {
-    if (number === 1) {
-      return 'I';
-    } else if (number === 5) {
-      return 'V';
-    } else if (number === 10) {
-      return 'X';
-    } else if (number === 50) {
-      return 'L';
-    } else if (number === 100) {
-      return 'C';
-    } else if (number === 500) {
-      return 'D';
-    } else if (number === 1000) {
-      return 'M';
+    let romanNumerals = {
+      1: 'I',
+      5: 'V',
+      10: 'X',
+      50: 'L',
+      100: 'C',
+      500: 'D',
+      1000: 'M'
     }
+
+    return romanNumerals[number];
+
   }
 }
 
