29731ebaaf8dc0ca31385b9074132c90fcd3e36c
Concha Asensio <concha@codium.team>
2023-05-30 20:33:28 +0200

    FizzBuzz should return Fizz instead of 6

--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -55,4 +55,13 @@ describe('FizzBuzz', () => {
     expect(fizzBuzzNumbers[2]).toBe('Fizz');
   });
 
+  it('should return Fizz instead of 6', () => {
+
+    const fizzBuzz = new FizzBuzz();
+
+    const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+
+    expect(fizzBuzzNumbers[5]).toBe('Fizz');
+  });
+
 });
