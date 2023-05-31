96e9d2047b575c1d5a3f1976872518ffe70de204
Concha Asensio <concha@codium.team>
2023-05-31 21:01:27 +0200

    reformat

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -1,23 +1,23 @@
 class FizzBuzz {
 
-    returnNumbers() {
+  returnNumbers() {
 
-        let numbers = [];
+    let numbers = [];
 
-        for (let index = 0; index < 100; index++) {
-            const number = index + 1;
+    for (let index = 0; index < 100; index++) {
+      const number = index + 1;
 
-            if (number % 3 === 0) {
-                numbers.push('Fizz')
-            } else if (number % 5 === 0) {
-                numbers.push('Buzz')
-            } else {
-                numbers.push(number.toString())
-            }
-        }
-
-        return numbers;
+      if (number % 3 === 0) {
+        numbers.push('Fizz');
+      } else if (number % 5 === 0) {
+        numbers.push('Buzz');
+      } else {
+        numbers.push(number.toString());
+      }
     }
+
+    return numbers;
+  }
 
 }
 
--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -1,73 +1,72 @@
-
 const FizzBuzz = require('../src/fizz_buzz');
 describe('FizzBuzz', () => {
-    let fizzBuzz;
-    beforeEach(() => {
-        fizzBuzz = new FizzBuzz();
-    })
-    it('should return more than one number', () => {
+  let fizzBuzz;
+  beforeEach(() => {
+    fizzBuzz = new FizzBuzz();
+  });
+  it('should return more than one number', () => {
 
-        const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+    const fizzBuzzNumbers = fizzBuzz.returnNumbers();
 
-        expect(Array.isArray(fizzBuzzNumbers)).toBe(true);
+    expect(Array.isArray(fizzBuzzNumbers)).toBe(true);
+  });
+
+  it('should return 100 elements', () => {
+
+    const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+
+    expect(fizzBuzzNumbers.length).toBe(100);
+  });
+
+  describe('FizzBuzz is a number when', () => {
+
+    const numbers = [
+      [0, '1'],
+      [1, '2'],
+      [3, '4'],
+    ];
+
+    it.each(numbers)('returns number %s', (position, number) => {
+
+      const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+
+      expect(fizzBuzzNumbers[position]).toBe(number);
+    });
+  });
+
+  describe('FizzBuzz is Fizz when', () => {
+
+    const numbers = [
+      [3],
+      [6],
+      [9],
+    ];
+
+    it.each(numbers)('returns Fizz instead of %s', (number) => {
+
+      const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+
+      let position = number - 1;
+      expect(fizzBuzzNumbers[position]).toBe('Fizz');
     });
 
-    it('should return 100 elements', () => {
+  });
 
-        const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+  describe('FizzBuzz is Buzz when', () => {
+    const numbers = [
+      [5],
+      [10],
+      [20],
+    ];
 
-        expect(fizzBuzzNumbers.length).toBe(100);
+    it.each(numbers)('returns Buzz instead of %s', (number) => {
+
+      const fizzBuzzNumbers = fizzBuzz.returnNumbers();
+
+      let position = number - 1;
+
+      expect(fizzBuzzNumbers[position]).toBe('Buzz');
     });
 
-    describe('FizzBuzz is a number when', () => {
-
-        const numbers = [
-            [0, '1'],
-            [1, '2'],
-            [3, '4']
-        ];
-
-        it.each(numbers)('returns number %s', (position, number) => {
-
-            const fizzBuzzNumbers = fizzBuzz.returnNumbers();
-
-            expect(fizzBuzzNumbers[position]).toBe(number);
-        });
-    })
-
-    describe('FizzBuzz is Fizz when', () => {
-
-        const numbers = [
-            [3],
-            [6],
-            [9]
-        ]
-
-        it.each(numbers)('returns Fizz instead of %s', (number) => {
-
-            const fizzBuzzNumbers = fizzBuzz.returnNumbers();
-
-            let position = number-1;
-            expect(fizzBuzzNumbers[position]).toBe('Fizz');
-        });
-
-    })
-
-    describe('FizzBuzz is Buzz when', () => {
-        const numbers = [
-            [5],
-            [10],
-            [20]
-        ]
-
-        it.each(numbers)('returns Buzz instead of %s', (number) => {
-
-            const fizzBuzzNumbers = fizzBuzz.returnNumbers();
-            let position = number-1;
-
-            expect(fizzBuzzNumbers[position]).toBe('Buzz');
-
-        });
-
-    })
+  });
 });
